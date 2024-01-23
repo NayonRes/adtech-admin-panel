1653b0d121d40ca9241fef5632f74901fece689d
Nayongithub <mahnayon@gmail.com>
2024-01-24 01:56:42 +0600

    design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Layout from "./pages/layout/Layout";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AuthContextProvider from "./context/AuthContext";
+import Slide from "@mui/material/Slide";
+import axios from "axios";
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9e1f63",
+      // main: "#353b48",
+      contrastText: "#fff",
+    },
+    info: {
+      main: "#0E6251",
+      contrastText: "#fff",
+    },
+    error: {
+      main: "#F91351",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    h1: {
+      [createTheme().breakpoints.down("xl")]: {
+        fontSize: "4rem",
+      },
+    },
+    h4: {
+      [createTheme().breakpoints.down("xl")]: {
+        fontSize: "1.75rem",
+      },
+    },
+  },
+});
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.common["Content-Type"] = "application/json";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {/* <div style={{ maxWidth: "1366px", margin: "auto" }}> */}
+
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider
+          maxSnack={1}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          TransitionComponent={Slide}
         >
-          Learn React
-        </a>
-      </header>
+          <BrowserRouter>
+            <AuthContextProvider>
+              <Layout />
+            </AuthContextProvider>
+          </BrowserRouter>
+        </SnackbarProvider>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/pages/navigation/Navigation.jsx
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+// import ForgotPassword from "../user-forms/ForgotPassword";
+import Login from "../user-forms/Login";
+// import ResetPassword from "../user-forms/ResetPassword";
+import Verify from "../user-forms/Verify";
+import { AuthContext } from "../../context/AuthContext";
+// import NoMatch from "../NoMatch";
+// import Dialog from "@mui/material/Dialog";
+// import DialogContent from "@mui/material/DialogContent";
+// import PulseLoader from "react-spinners/PulseLoader";
+// import UserList from "../users/UserList";
+// import Dashboard from "../dashboard/Dashboard";
+
+function PrivateRoute({ children }) {
+  const { tuso_admin_panel } = useContext(AuthContext);
+
+  return tuso_admin_panel.token ? children : <Navigate to="/" />;
+}
+function RedirectToHome({ children }) {
+  const { tuso_admin_panel } = useContext(AuthContext);
+
+  return !tuso_admin_panel.token ? children : <Navigate to="/dashboard" />;
+}
+const Navigation = () => {
+  const { tuso_admin_panel } = useContext(AuthContext);
+
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RedirectToHome>
+              <Login />
+            </RedirectToHome>
+          }
+        />
+        <Route
+          path="verify"
+          element={
+            <RedirectToHome>
+              <Verify />
+            </RedirectToHome>
+          }
+        />
+        {/* <Route
+          path="dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        /> */}
+        {/* <Route path="forgot-password" element={<ForgotPassword />} /> */}
+        {/* <Route
+          path="change-password"
+          element={
+            <PrivateRoute>
+              <ResetPassword />
+            </PrivateRoute>
+          }
+        /> */}
+        {/* <Route
+          path="user-list"
+          element={
+            <PrivateRoute>
+              <UserList />
+            </PrivateRoute>
+          }
+        /> */}
+
+        {/* <Route
+          path="*"
+          element={!tuso_admin_panel.token ? <Navigate to="/" /> : <NoMatch />}
+        /> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default Navigation;
